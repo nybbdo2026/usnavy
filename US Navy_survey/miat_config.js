147bc41394b74dc2f1eae41f296a972d577e55b0
Prabhakar Mahapatra (Omnicom) <prabhakar.mahapatra@omc.com>
2026-05-08 23:03:51 +0530

    Initial commit

---
+++ US Navy_survey/miat_config.js
@@ -0,0 +1,42 @@
+const brands = [
+    { name: "Air Force", img: "img/Air Force.png" },
+    { name: "Army", img: "img/Army.png" }, 
+    { name: "Coast Guard", img:"img/Coast Guard.png" }, 
+    { name: "Marine Corps.png", img:"img/Marine Corps.png" },
+    { name: "Navy", img: "img/Navy.png" },
+    { name: "Space Force.png", img: "img/Space Force.png" }
+  ];
+
+const category = "Navy"
+
+const attributes = ["adventure","freedom","courage","daring","discovery","fighting spirit","risk","resilience","pride","perfection","recognition","success","achievement","self-confidence","improve myself","leadership","determination","discipline","control","reason","efficiency","reliability","expertise","order","precision","safety","security","caring","closeness","comradeship","protection","feel at home","belonging","joy","treat","allure","exhilaration","appreciation","optimism","authenticity","satisfaction","excitement","zest for life","inspiration","vitality","drive","individuality","exploration","innovation"];
+
+const pretest_attributes = [
+  "Hot", 
+  "Cold", 
+  "Dangerous",
+  "Refreshing", 
+  "Cute",
+  "Soft"
+];
+
+
+const pretest_images = [
+  {name: "Cat", img: "pretest_img/pretest_cat.png", correct: ["Cute", "Soft"]},
+  {name: "Fire", img:"pretest_img/pretest_fire.png", correct:["Dangerous", "Hot"]}, 
+  {name: "Icecube", img: "pretest_img/pretest_icecube.png", correct:["Cold", "Refreshing"]}
+]
+
+const pretest_attributes_multiple = ["Hot", "Cold", "Dangerous", "Refreshing", "Cute", "Soft", "Travel", "Excitement", "Precise", "Time", "Deep", "Wet"]
+
+const pretest_images_multiple = [
+  {name: "Cat", img: "pretest_img/pretest_cat.png", correct: ["Cute", "Soft"]},
+  {name: "Fire", img:"pretest_img/pretest_fire.png", correct:["Dangerous", "Hot"]}, 
+  {name: "Icecube", img: "pretest_img/pretest_icecube.png", correct:["Cold", "Refreshing"]}, 
+  {name: "Driving", img: "pretest_img/pretest_driving.png", correct:["Travel", "Excitement"]}, 
+  {name: "Ocean", img: "pretest_img/pretest_ocean.png", correct:["Deep", "Wet"]},
+  {name: "Clock", img: "pretest_img/pretest_clock.png", correct:["Precise", "Time"]}
+]
+
+
+const survey_name = "US Navy"
